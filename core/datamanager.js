cc00a662e82186b467e3d850dd4018d50bfbd069
rarufino27-alt <rarufino27@gmail.com>
2026-02-18 08:41:07 -0300

    Update datamanager.js

--- core/datamanager.js
+++ core/datamanager.js
@@ -2,23 +2,23 @@ const DataManager = {
   rotas: [],
 
   arquivos: [
-  "./data/condominio-porto-do-cabo.json",
-  "./data/lote-garapu2-lote-dona-amara.json",  
-  "./data/cohab.json",
-  "./data/centro-do-cabo.json",
-  "./data/shopping-costa-dourada.json",
-  "./data/aguia-american-club-br-101.json",
-  "./data/empresas.json",
-  "./data/engenhos.json",
-  "./data/hospitais-clinicas.json",
-  "./data/interurbanas.json",
-  "./data/interestaduais.json",
-  "./data/lazer-festa.json",
-  "./data/locais.json",
-  "./data/longas-locais.json",
-  "./data/praias.json",  
-  "./data/bairro-sao-francisco-baixo.json"
-],
+    "./data/condominio-porto-do-cabo.json",
+    "./data/lote-garapu2-lote-dona-amara.json",
+    "./data/cohab.json",
+    "./data/centro-do-cabo.json",
+    "./data/shopping-costa-dourada.json",
+    "./data/aguia-american-club-br-101.json",
+    "./data/empresas.json",
+    "./data/engenhos.json",
+    "./data/hospitais-clinicas.json",
+    "./data/interurbanas.json",
+    "./data/interestaduais.json",
+    "./data/lazer-festa.json",
+    "./data/locais.json",
+    "./data/longas-locais.json",
+    "./data/praias.json",
+    "./data/bairro-sao-francisco-baixo.json"
+  ],
 
   async carregar() {
     try {
@@ -40,8 +40,7 @@ const DataManager = {
   },
 
   listarOrigens() {
-    const origens = [...new Set(this.rotas.map(r => r.origem))];
-    return origens.sort();
+    return [...new Set(this.rotas.map(r => r.origem))].sort();
   },
 
   listarDestinos(origem) {
@@ -55,18 +54,31 @@ const DataManager = {
       r => r.origem === origem && r.destino === destino
     );
 
-    // rota invertida (ida/volta)
     if (!rota) {
       rota = this.rotas.find(
         r => r.origem === destino && r.destino === origem
       );
     }
 
-    return rota ? rota.valor : null;
+    return rota ? Number(rota.valor) : null;
+  },
+
+  calcularValorCompleto(origem, parada, destino) {
+
+    if (!origem || !destino) return null;
+
+    // sem parada
+    if (!parada) {
+      return this.buscarValor(origem, destino);
+    }
+
+    // com parada
+    const trecho1 = this.buscarValor(origem, parada);
+    const trecho2 = this.buscarValor(parada, destino);
+
+    if (trecho1 === null || trecho2 === null) return null;
+
+    return trecho1 + trecho2;
   }
 };
-
-
-
-
 
